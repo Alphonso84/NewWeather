20ecd398cc24eb7d12c0cfd00692ca457e570f8d
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2023-11-15 22:11:07 -0800

    Initial Commit

---
+++ NewWeather/NewWeatherCollectionView.swift
@@ -0,0 +1,8 @@
+//
+//  NewWeatherCollectionView.swift
+//  NewWeather
+//
+//  Created by Alphonso Sensley II on 11/15/23.
+//
+
+import Foundation
---
+++ NewWeather/NewWeatherViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  NewWeatherViewCell.swift
+//  NewWeather
+//
+//  Created by Alphonso Sensley II on 11/15/23.
+//
+
+import Foundation
